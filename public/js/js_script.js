bbf3dacf0d86003a38cab45014c0832ff04001a7
axli7328 <axellingestal@live.se>
2020-03-20 13:38:08 +0100

    Task 4-8 completed.

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,80 +1,165 @@
-function menuItem (name, ingredients, kCal, allergy, imagePath) {
+function menuItem (name, ingredients, kCal, gluten, lactose, imgPath) {
     this.name = name;
     this.ingredients = ingredients;
     this.calories = kCal;
-    this.allergy = allergy;
-    this.imagePath = imagePath;
+    this.gluten = gluten;
+    this.lactose = lactose;
+    this.imgPath = imgPath;
     this.info = function() {
-        return this.name + ' ' + this.calories;
+        return this.name + ' ' + this.calories + this.gluten;
     };
 }
 
 let B1 = new menuItem (
     'The Burger Dream',
     'Contains meat, not for vegans',
-    'Calories, it\'s over 9000!!!',
-    'Contains gluten and lactose, but it\'s worth it',
+    9000,
+    true,
+    true,
     'https://www.peoplemagazine.co.za/app/uploads/2018/08/burger-patty.jpg',
 )
 
 let B2 = new menuItem (
     'The Vegan Bune',
     'Contains beans by all means',
-    'Healthy amount of calories due to vegan',
-    'Contains no gluten, nolactose, no nothing',
+    100,
+    true,
+    true,
     'https://nutritionrefined.com/wp-content/uploads/2017/01/veggie-burger-vegan-gluten-free-healthy-grillable.jpg'
 )
 
 let B3 = new menuItem (
     'Still better than Donken Style',
     'Contains an unhealthy amount of mold',
-    'Does calories matter when eating mold? Didn\'t think so',
-    'Contains less gluten than before',
+    200,
+    true,
+    true,
     'https://ima.xgoo.jp/column/img2/oosumi/20150409_burger_end_010.jpg'
 )
 
 let B4 = new menuItem (
     'Chicken for chickens',
     'Contains a bird',
-    '15',
-    'Contains gluten and lactose',
+    15,
+    true,
+    true,
     'https://www.kitchensanctuary.com/wp-content/uploads/2017/08/Crispy-Chicken-Burger-with-Honey-Mustard-Coleslaw-recipe-tall-FS.webp'
 )
 
 let B5 = new menuItem (
     'Halloumi Heaven',
     'Contains cheese',
-    '500',
-    'Contains gluten and lactose',
+    500,
+    true,
+    false,
     'https://www.sainsburysmagazine.co.uk/uploads/media/675x882/00/5870-Peri-peri-halloumi-burger-with-sweetcorn-salsa.jpg?v=1-0'
 )
 
-let id = document.getElementById("myID");
+menuItem.prototype.getName = function getName() {
+  return this.name;
+};
+menuItem.prototype.getLactoseInfo = function getLactoseInfo() {
+  	if (this.lactose == true) {
+  		return ", Contains lactose";	  
+    }
+};
+menuItem.prototype.getGlutenInfo = function getGlutenInfo() {
+		if (this.gluten == true) {
+  		return ", Contains gluten";	  
+    }
+};
+menuItem.prototype.getKcal = function getKcal() {
+    return this.calories;
+};
+menuItem.prototype.getImgPath = function getImgPath() {
+    return this.imgPath;
+};
 
-let p_1 = document.createElement("p");
-let p_2 = document.createElement("p");
-let p_3 = document.createElement("p");
-let p_4 = document.createElement("p");
-let p_5 = document.createElement("p");
+// let id = document.getElementById("myID");
 
-let B1 = document.createTextNode(B1.info());
-let B2 = document.createTextNode(B2.info());
-let B3 = document.createTextNode(B3.info());
-let B4 = document.createTextNode(B4.info());
-let B5 = document.createTextNode(B5.info());
+// let p_1 = document.createElement("p");
+// let p_2 = document.createElement("p");
+// let p_3 = document.createElement("p");
+// let p_4 = document.createElement("p");
+// let p_5 = document.createElement("p");
 
-p_1.appendChild(B1);
-p_2.appendChild(B2);
-p_3.appendChild(B3);
-p_4.appendChild(B4);
-p_5.appendChild(B5);
+// let B1 = document.createTextNode(B1.info());
+// let B2 = document.createTextNode(B2.info());
+// let B3 = document.createTextNode(B3.info());
+// let B4 = document.createTextNode(B4.info());
+// let B5 = document.createTextNode(B5.info());
 
-id.appendChild(p_1);
-id.appendChild(p_2);
-id.appendChild(p_3);
-id.appendChild(p_4);
-id.appendChild(p_5);
+// p_1.appendChild(B1);
+// p_2.appendChild(B2);
+// p_3.appendChild(B3);
+// p_4.appendChild(B4);
+// p_5.appendChild(B5);
 
-console.log(B1.info() + B2.info() + B3.info() + B4.info() + B5.info()); 
+// id.appendChild(p_1);
+// id.appendChild(p_2);
+// id.appendChild(p_3);
+// id.appendChild(p_4);
+// id.appendChild(p_5);
 
 
+// console.log(B1.info() + B2.info() + B3.info() + B4.info() + B5.info()); 
+
+
+
+
+
+
+
+
+
+
+// function menuItem (name, ingredients, kCal, allergy, imagePath) {
+//     this.name = name;
+//     this.ingredients = ingredients;
+//     this.calories = kCal;
+//     this.allergy = allergy;
+//     this.imagePath = imagePath;
+//     this.info = function() {
+//         return this.name + ' ' + this.calories;
+//     };
+// }
+
+// let B1 = new menuItem (
+//     'The Burger Dream',
+//     'Contains meat, not for vegans',
+//     'Calories, it\'s over 9000!!!',
+//     'Contains gluten and lactose, but it\'s worth it',
+//     'https://www.peoplemagazine.co.za/app/uploads/2018/08/burger-patty.jpg',
+// )
+
+// let B2 = new menuItem (
+//     'The Vegan Bune',
+//     'Contains beans by all means',
+//     'Healthy amount of calories due to vegan',
+//     'Contains no gluten, nolactose, no nothing',
+//     'https://nutritionrefined.com/wp-content/uploads/2017/01/veggie-burger-vegan-gluten-free-healthy-grillable.jpg'
+// )
+
+// let B3 = new menuItem (
+//     'Still better than Donken Style',
+//     'Contains an unhealthy amount of mold',
+//     'Does calories matter when eating mold? Didn\'t think so',
+//     'Contains less gluten than before',
+//     'https://ima.xgoo.jp/column/img2/oosumi/20150409_burger_end_010.jpg'
+// )
+
+// let B4 = new menuItem (
+//     'Chicken for chickens',
+//     'Contains a bird',
+//     '15',
+//     'Contains gluten and lactose',
+//     'https://www.kitchensanctuary.com/wp-content/uploads/2017/08/Crispy-Chicken-Burger-with-Honey-Mustard-Coleslaw-recipe-tall-FS.webp'
+// )
+
+// let B5 = new menuItem (
+//     'Halloumi Heaven',
+//     'Contains cheese',
+//     '500',
+//     'Contains gluten and lactose',
+//     'https://www.sainsburysmagazine.co.uk/uploads/media/675x882/00/5870-Peri-peri-halloumi-burger-with-sweetcorn-salsa.jpg?v=1-0'
+// )
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,10 +1,47 @@
 const vm = new Vue({
-  el: '#myID',
-  data: {
-    B1: B1.info(),
-    B2: B2.info(),
-    B3: B3.info(),
-    B4: B4.info(),
-    B5: B5.info(),
-  }
-})
+    el: "#wrapper",
+    data: {
+        header: "Select Burger",
+        menu: "Here you select which wonderful burger you want",
+        gluten: "Contains gluten",
+        lactose: "Contians lacose",
+        burgers: [
+  	        {
+                name: B1.getName(),
+                lactose: B1.getLactoseInfo(),
+                gluten: B1.getGlutenInfo(),
+                kCal: B1.getKcal(),
+                imgPath: B1.getImgPath()
+            },
+            {
+                name: B2.getName(),
+                lactose: B2.getLactoseInfo(),
+                gluten: B2.getGlutenInfo(),
+                kCal: B2.getKcal(),
+                imgPath: B2.getImgPath()
+            },
+            {
+                name: B3.getName(),
+                lactose: B3.getLactoseInfo(),
+                gluten: B3.getGlutenInfo(),
+                kCal: B3.getKcal(),
+                imgPath: B3.getImgPath()
+            },
+            {
+                name: B4.getName(),
+                lactose: B4.getLactoseInfo(),
+                gluten: B4.getGlutenInfo(),
+                kCal: B4.getKcal(),
+                imgPath: B4.getImgPath()
+            },
+            {            
+                name: B5.getName(),
+                lactose: B5.getLactoseInfo(),
+                gluten: B5.getGlutenInfo(),
+                kCal: B5.getKcal(),
+                imgPath: B5.getImgPath()
+            }
+        ]
+    },
+    methods: {}
+});
